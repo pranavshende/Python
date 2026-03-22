abb88336d6b0ffbe4410c361742b0f1d7b7e9d34
Pranav shende <pranavshende@users.noreply.github.com>
2026-03-22 20:08:03 +0530

    d

---
+++ ControlStatements/ConditionalStatements.py
@@ -0,0 +1,12 @@
+num = 10
+if num in range(1,11):
+    print("num is in range")
+else:
+    print("Num is out of range")
+#OR
+
+num = 10
+if num < 11:
+    print("num is in range")
+else:
+    print("Num is out of range")
